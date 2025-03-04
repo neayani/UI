78b10bd6217040dfaf3924b6cf99d582e54050c1
neayani <neayani@gmail.com>
2025-03-04 10:34:24 +0330

    update

--- src/components/ListUsers.jsx
+++ src/components/ListUsers.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import Register from './Register';
 //import axios from "axios";
 
 const ListUsers = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true); // Loading state
+  const[showRegister,setShowRegister]=useState(false);
+  const handleCreateClick = () => {
+    console.log('Creating new user...');
+    setShowRegister(true);  // Show the Register component when clicked
+ 
+  };
+  console.log('showRegister state:', showRegister);  // Log the state value
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -32,12 +40,19 @@ const ListUsers = () => {
   if (loading) {
     return <div>Loading...</div>;
   }
-  if (!loading) {
-    return (
-     
+
+  return (
+    <div>
+      {!showRegister ? (
+        <>
+        {
+          (!loading) &&
+       
       <div className="container mt-4">
-      <button className="btn btn-success" onClick={handleCreate}>Create User</button>
-        <h1>User List</h1>
+      <button className="btn btn-success" onClick={handleCreateClick}>Create User</button>
+      {showRegister && <Register/> }
+
+      <h1>User List</h1>
         <table className="table table-bordered">
           <thead>
             <tr>
@@ -85,17 +100,92 @@ const ListUsers = () => {
             ))}
           </tbody>
         </table>
-      </div>
-    );
-  }
-};
-function handleCreate()
-{
+      
 
+      
+     
+      </div>
 }
+        </>
+          
+      )
+      :
+
+      (<Register/>)
+    }
+    </div>
+  )
+
+
+
+  // if (!loading && !showRegister) {
+  //   return (
+     
+  //     <div className="container mt-4">
+  //     <button className="btn btn-success" onClick={handleCreateClick}>Create User</button>
+  //     {showRegister && <Register/> }
+
+  //     <h1>User List</h1>
+  //       <table className="table table-bordered">
+  //         <thead>
+  //           <tr>
+  //             {/* <th>ID</th> */}
+  //             <th>Name</th>
+  //             <th>LastName</th>
+  //             <th>UserName</th>
+  //             <th>Email</th>
+  //             <th>IsActive</th>
+  //             <th>isAdmin</th>
+  //             <th></th>
+  //             <th></th>
+  //           </tr>
+  //         </thead>
+  //         <tbody>
+  //           {users.map((user) => (
+  //             <tr key={user.id}>
+  //               {/* <td>{user.id}</td> */}
+  //               <td>
+  //                 <input type="text" value={user.name}></input>
+  //               </td>
+  //               <td>
+  //                 <input type="text" value={user.lastName}></input>
+  //               </td>
+
+  //               <td>
+  //                 <input type="text" value={user.userName}></input>
+  //               </td>
+  //               <td>
+  //                 <input type="text" value={user.email}></input>
+  //               </td>
+  //               <td>
+  //                 <input type="checkbox" checked={user.isActive}></input>
+  //               </td>
+  //               <td>
+  //                 <input type="checkbox" checked={user.isAdmin}></input>
+  //               </td>
+  //               <td>
+  //                 <button className="btn btn-info btn-sm" onClick={handleUpdate}>Update</button>
+  //               </td>
+  //               <td>
+  //                 <button className="btn btn-warning btn-sm" onClick={handleDelete}>Delete</button>
+  //               </td>
+  //             </tr>
+  //           ))}
+  //         </tbody>
+  //       </table>
+      
+
+      
+     
+  //     </div>
+  //   );
+  // }
+
+};
+
 function handleUpdate(user)
 {
-const res=axios.put("https://localhost:7101/api/User",user);
+//const res=axios.put("https://localhost:7101/api/User",user);
 
 }
 function handleDelete()
